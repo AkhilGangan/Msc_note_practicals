1e97d5acaf8646676a86618a1b7244fd119e7328
Akhil Ravindra Gangan <111442480+AkhilGangan@users.noreply.github.com>
2023-04-17 22:44:58 +0530

    Add files via upload

---
+++ Big data practical_6.R
@@ -0,0 +1,17 @@
+install.packages("tm")
+require("tm")
+install.packages("devtools")
+readinteger <-function()
+{
+  n<-readline(prompt="Enter value of k-1:")
+  k<- as.integer(n)
+  u1<- readLines("C:/MSC Notes/file.txt")
+  Shingle <-0
+  i<-0
+  while(i<nchar(u1)-k+1){
+  Shingle[i] <- substr(u1,start=i,stop=i+k)
+  print(Shingle[i])
+  i=i+1         
+  }
+}  
+if(interactive())readinteger()  
